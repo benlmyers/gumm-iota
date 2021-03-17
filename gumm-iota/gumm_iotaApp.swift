68e05ed31484fd1b2b270e7f8cca3025773d4cd6
Ben Myers <benben28405678@gmail.com>
2021-03-16 23:45:33 -0400

    Initial Commit

---
+++ gumm-iota/gumm_iotaApp.swift
@@ -0,0 +1,17 @@
+//
+//  gumm_iotaApp.swift
+//  gumm-iota
+//
+//  Created by Ben Myers on 3/16/21.
+//
+
+import SwiftUI
+
+@main
+struct gumm_iotaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
